ee4dcc6afd58460931d4a88eb865f520967046bb
Oluwatosin Jeremiah <jerexbambex@gmail.com>
2019-07-15 04:10:24 -0700

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,15 @@
 for(let m = 1; m <= 100; m++) {
-		switch(true) {
-			case (m % 3) === 0 && (m % 5) === 0:
-				console.log("FizzBuzz");
-				break;
-			case (m % 3) === 0:
-				console.log("Fizz");
-				break;
-			case (m % 5) === 0:
-				console.log("Buzz");
-				break;
-			default :
-				console.log(m);
-		}
-	}
+  switch(true) {
+    case (m % 3) === 0 && (m % 5) === 0:
+	console.log("FizzBuzz");
+	break;
+    case (m % 3) === 0:
+	console.log("Fizz");
+	break;
+    case (m % 5) === 0:
+	console.log("Buzz");
+	break;
+    default :
+	console.log(m);
+  }
+}
